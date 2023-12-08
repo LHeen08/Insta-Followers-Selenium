478a94ba2644e26f22a9b242276873bc1b7244c5
LHeen08 <lucasheenan21@gmail.com>
2023-12-08 15:08:58 -0500

    changes for more consistent fetching of data, better xpath

--- bot.py
+++ bot.py
@@ -41,7 +41,6 @@ def login(driver):
     username_input = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
     password_input = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
 
-
     # Now clear the inputs and send keys
     username_input.clear()
     username_input.send_keys(username)
@@ -54,18 +53,48 @@ def login(driver):
     time.sleep(5) # wait a bit for the page to load to login
 
 
+def get_followers(driver):
+    driver.get(f'https://www.instagram.com/{username}/') # navigate to the user page and get the followers
+    time.sleep(2)
 
-    while True:
-        time.sleep(10)
+    # Retrieve the total number of followers
+    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//span[@class='_ac2a _ac2b']")))
+    followers_number = element.text
+    print("total followers: ", followers_number)  # print number of followers to screen
 
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//a[contains(@href, '/followers')]"))).click()
+    time.sleep(2)
+    print(f"Getting all followers for {username}")
 
+    users = set() # set to hold followers
 
+    while len(users) < 20:
+        followers = driver.find_elements(By.XPATH, "//a[contains(@href, '/')]")
+
+        for i in followers:
+            if i.get_attribute('href'):
+                users.add(i.get_attribute('href').split("/")[3])
+            else:
+                continue
+
+        ActionChains(driver).send_keys(Keys.END).perform()
+        time.sleep(1)
+
+    users = list(users)[:20]  # Trim the user list to match the desired number of followers
+
+    print(f"[Info] - Saving followers for {username}...")
+    with open(f'{username}_followers.txt', 'a') as file:
+        file.write('\n'.join(users) + "\n")
 
 
 
 # Main driver 
 if __name__ == '__main__':
     login(driver)
+    get_followers(driver)
+
+
+
 
 # #   Function to try for element returning exception if not found
 # #   Parameters are a string, and type of element (xpath or css)
